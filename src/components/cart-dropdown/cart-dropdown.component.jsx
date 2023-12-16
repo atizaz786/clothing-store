16985a5fc139528a032a12b3aff301e633460978
atizaz786 <atizazhashmi786@gmail.com>
2023-12-16 10:50:11 +0000

    feat: Added cart functionality with context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,9 @@
-import { React } from 'react'
+import { React, useContext } from 'react'
 
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+
+import { CartContext } from '../../contexts/cart.context'
 
 
 import './cart-dropdown.styles.scss'
@@ -8,9 +11,13 @@ import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
 
+    const { cartItems } = useContext(CartContext)
+
     return (
         <div className='cart-dropdown-container'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+           {cartItems.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}
+        </div>
         <Button>Go to Checkout</Button>
 
         </div>
